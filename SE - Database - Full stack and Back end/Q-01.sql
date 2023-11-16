3b71a227461fadd5f810009756c342ae9645ffc9
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-16 14:41:44 +0530

    .....

---
+++ SE - Database - Full stack and Back end/Q-01.sql
@@ -0,0 +1,27 @@
+-- # Write SQL query to solve the problem given below 
+-- Make sure to user right sql syntax to solve the query given below :
+-- ● Write sql query to find the items whose prices are higher than or equal 250rs. 
+-- Order the result by product price in descending, then product name in 
+-- ascending. Return pro_name and pro_price 
+
+-- ● Write a sql query to find the cheapest item. Return pro_name and pro_price.
+
+-- ● Write the sql query to calculate the average price of the items for each company. Return average price and company code.
+
+-- ● Write the sql query to find the average total for all the product mention in the table 
+
+CREATE TABLE product (
+    product_id INT PRIMARY KEY,
+    pro_name VARCHAR(255),
+    pro_price DECIMAL(10, 2),
+    pro_code VARCHAR(10),
+    company_code VARCHAR(10)
+);
+
+-- Sample data insertion for testing
+INSERT INTO product (product_id, pro_name, pro_price, pro_code, company_code) VALUES
+(1, 'Keyboard', 300.00, 'K123', 'C1'),
+(2, 'Motherboard', 500.00, 'M456', 'C2'),
+(3, 'Monitor', 350.00, 'M789', 'C1'),
+(4, 'Speaker', 200.00, 'S012', 'C3');
+x`
