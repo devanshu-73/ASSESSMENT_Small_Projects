33c69f4b096260240ccd877f49f2a7aeea3f17a5
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-16 15:00:37 +0530

    ...

--- SE - Database - Full stack and Back end/Q-01.sql
+++ SE - Database - Full stack and Back end/Q-01.sql
@@ -22,19 +22,17 @@ CREATE TABLE product (
     company_code VARCHAR(10)
 );
 
--- Sample data insertion for testing
-insert into product (product_id, pro_name, pro_price, pro_code) values
-(101, 'Mother Board', 3200.00,15),
-(102, 'Key Board', 450.00,16),
+INSERT INTO product (product_id, pro_name, pro_price, pro_code) VALUES
+(101, 'Mother Board', 3200.00, 15),
+(102, 'Key Board', 450.00, 16),
 (103, 'Zip Drive', 250.00, 14),
-(104, 'Speaker', 550.00,16 ),
+(104, 'Speaker', 550.00, 16),
 (105, 'Monitor', 5000.00, 11),
-(106, 'DVD Drive', 900.00, 12);
-(107, 'CD Drive', 800.00,12 );
-(108, 'Printer', 2600.00, 13);
-(109, 'Refill Cartridge', 350.00, 13);
+(106, 'DVD Drive', 900.00, 12),
+(107, 'CD Drive', 800.00, 12),
+(108, 'Printer', 2600.00, 13),
+(109, 'Refill Cartridge', 350.00, 13),
 (110, 'Mouse', 250.00, 12);
-
 
 -- Write SQL query to find the items whose prices are higher than or equal 250rs. Order the result by product price in descending, then product name in ascending. Return pro_name and pro_price.
 
