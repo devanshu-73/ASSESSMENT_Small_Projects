3a741cdc39d124f42e9eed7ce80ad9895da9bfd2
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-08 04:56:44 +0530

    ............

--- Python - Advance python programming/Customer.py
+++ Python - Advance python programming/Customer.py
@@ -1,18 +1,18 @@
-class Base:
-    def __init__(self, name, age, gender, balance):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self._balance = balance  # Private field
+# class Base:
+#     def __init__(self, name, age, gender, balance):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#         self._balance = balance  # Private field
 
-    def display_details(self):
-        print(f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nBalance: {self._balance}")
+#     def display_details(self):
+#         print(f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}\nBalance: {self._balance}")
 
-class Customer(Base):
-    def __init__(self, name, age, gender, balance, room_number):
-        super().__init__(name, age, gender, balance)
-        self.room_number = room_number
+# class Customer(Base):
+#     def __init__(self, name, age, gender, balance, room_number):
+#         super().__init__(name, age, gender, balance)
+#         self.room_number = room_number
 
-    def display_details(self):
-        super().display_details()
-        print(f"Room Number: {self.room_number}")
+#     def display_details(self):
+#         super().display_details()
+#         print(f"Room Number: {self.room_number}")
--- Python - Advance python programming/Manager.py
+++ Python - Advance python programming/Manager.py
@@ -1,21 +1,21 @@
-from DB_Connection import Database
-from Customer import Customer
+# from DB_Connection import Database
+# from Customer import Customer
 
-class HotelManager:
-    def __init__(self):
-        self.db = Database() # Database Object as db
+# class HotelManager:
+#     def __init__(self):
+#         self.db = Database() # Database Object as db
 
-    def add_customer(self, name, age, gender, balance, room_number):
-        customer = Customer(name, age, gender, balance, room_number)
-        query = "insert into customers (name, age, gender, balance, room_number) values (%s, %s, %s, %s, %s)"
-        values = (customer.name, customer.age, customer.gender, customer._balance, customer.room_number)
-        self.db.execute_query(query, values)
-        print("Customer added successfully!")
+#     def add_customer(self, name, age, gender, balance, room_number):
+#         customer = Customer(name, age, gender, balance, room_number)
+#         query = "insert into customers (name, age, gender, balance, room_number) values (%s, %s, %s, %s, %s)"
+#         values = (customer.name, customer.age, customer.gender, customer._balance, customer.room_number)
+#         self.db.execute_query(query, values)
+#         print("Customer added successfully!")
 
-    def display_customers(self):
-        query = "select * from customers"
-        customers = self.db.fetch_data(query)
-        for customer in customers:
-            print(customer)
+#     def display_customers(self):
+#         query = "select * from customers"
+#         customers = self.db.fetch_data(query)
+#         for customer in customers:
+#             print(customer)
 
-# You can add more methods for other functionalities
+# # You can add more methods for other functionalities
