d7902694fa54d173455ed2b1620be79f28ee1202
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-01-27 16:20:47 +0530

    Assessment

--- Python - Advance python programming/HotelFunctions.py
+++ Python - Advance python programming/HotelFunctions.py
@@ -1,21 +1,21 @@
-# from Manager import HotelManager
+from Manager import HotelManager
 
-# class HotelFunctions:
-#     def __init__(self):
-#         self.manager = HotelManager()
+class HotelFunctions:
+    def __init__(self):
+        self.manager = HotelManager()
 
-#     def check_in(self):
-#         # Add logic for the check-in functionality
-#         pass
+    def check_in(self):
+        # Add logic for the check-in functionality
+        pass
 
-#     def show_guest_list(self):
-#         # Add logic for displaying the guest list
-#         pass
+    def show_guest_list(self):
+        # Add logic for displaying the guest list
+        pass
 
-#     def check_out(self):
-#         # Add logic for the check-out functionality
-#         pass
+    def check_out(self):
+        # Add logic for the check-out functionality
+        pass
 
-#     def get_guest_info(self):
-#         # Add logic for getting guest information
-#         pass
+    def get_guest_info(self):
+        # Add logic for getting guest information
+        pass
