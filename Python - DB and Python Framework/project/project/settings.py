d4fab51d36b4c2d8b97e17dbbc4235a3f2d2b341
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-20 20:05:54 +0530

    Settings

--- Python - DB and Python Framework/project/project/settings.py
+++ Python - DB and Python Framework/project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
