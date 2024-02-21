3cbdfea9ee4496ee8b3217ab6910aa607c4becac
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-21 18:15:10 +0530

    Assessment

--- Python - DB and Python Framework/project/app/admin.py
+++ Python - DB and Python Framework/project/app/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Club)
+admin.site.register(Book)
