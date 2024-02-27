39e8c557f3c041b63ab3c95ddca9b536e7a67263
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-27 19:49:08 +0530

    Assessment

--- Python - DB and Python Framework/institute_management/institute_app/urls.py
+++ Python - DB and Python Framework/institute_management/institute_app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from institute_app import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('home/',views.home,name='home'),
-    path('',views.login,name='login'),
+    path('', views.index, name='index'),  # URL for both login and home page
+    path('login/', views.login, name='login'),  # URL for login
+    path('logout/', views.logout, name='logout'),  # URL for logout
 ]
 
 
--- Python - DB and Python Framework/institute_management/institute_app/views.py
+++ Python - DB and Python Framework/institute_management/institute_app/views.py
@@ -1,68 +1,42 @@
-from django.shortcuts import render,redirect,HttpResponseRedirect
-from django.urls import reverse
-import random
-from .models import * 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import People
 
-# Create your views here.
-
-# function for fetching person data
-
-def data(f_username,f_password=None):
-   # getting all the user data
-   user=People.objects.get(username=f_username)
-   print("user",user)
-   if f_password:
-      user=People.objects.get(username=f_username, password=f_password)      
-   # creating a context for html
-   context={
-            "user":user,
-         }
-   return context
-
-def home(request):
-   if 'username' in request.session:
-      context=data(request.session['username'])   
-      return render(request, 'institute_app/index.html',context)
-   
-   return render(request, "institute_app/login.html")
-
+def index(request):
+    if 'username' in request.session:
+        # User is logged in, render home page
+        username = request.session['username']
+        try:
+            user = People.objects.get(username=username)
+            context = {"user": user}
+            return render(request, 'institute_app/index.html', context)
+        except People.DoesNotExist:
+            # If session username doesn't match any user, clear the session and render login
+            del request.session['username']
+    # User is not logged in, render login page
+    return render(request, "institute_app/login.html")
 
 def login(request):
-   # if the user is already logged-in
-   if 'username' in request.session:
-      context=data(request.session['username'])   
-      return render(request, 'institute_app/index.html',context)
-   else:
-      msg=None
-      try:
-         # getting user input from html
-         print("=========>>> Login")
-         user_username=request.POST.get('username')
-         user_password=request.POST.get('password')
-         print("=========>>>",user_username,user_password)
-         
-         if(user_username != None and user_password != None):
-            # if the user input is correct get the user data as an object
-            context=data(user_username,user_password)
+    if 'username' in request.session:
+        return redirect('index')  # Redirect to index if user is already logged in
 
-            # creating a session, so the institute_app have an info that the user is logged-in or not
-            request.session['username']=context['user'].username
-            
-            # rendering the dashboard
-            print("========== render index")
-            return render(request, 'institute_app/index.html',context)
-         
-      except Exception as e:
-         print("===========>>> Error =",e)
-         msg="Invalid Input Pls Enter Again"
-         
-      return render(request, "institute_app/login.html", {'msg':msg})
-   
-
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if username and password:
+            try:
+                user = People.objects.get(username=username, password=password)
+                request.session['username'] = user.username
+                return redirect('index')
+            except People.DoesNotExist:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Please provide both username and password.')
+    
+    return redirect('index')  # Redirect to index in case of GET request or login failure
 
 def logout(request):
-   print("==============>>> logout")
-   if "username" in request.session:
-      # deleting the session to notify that the user is logged out
-      del request.session['username']
-   return redirect(reverse('login'))
+    if "username" in request.session:
+        del request.session['username']
+    return redirect('index')
