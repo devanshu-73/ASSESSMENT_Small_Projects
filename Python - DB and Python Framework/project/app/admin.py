7077cc67de8061096db15cb7822089dd8c06583e
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-29 20:11:17 +0530

    Delete

--- Python - DB and Python Framework/project/app/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import *
-
-admin.site.register(People)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Club)
-admin.site.register(Book)
